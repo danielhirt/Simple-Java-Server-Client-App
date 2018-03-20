fffba09f16ef97d401b1b95714267bed261ae7ef
Daniel Christopher Hirt <dhirt@uncc.edu>
2018-03-20 15:25:39 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package midterm;
+package app;
 
 /**
  *
  * @author Daniel Hirt
- * @description ITSC 3166 Midterm Project
+ * @description Client/Server App
  * @file Server.java
  * @functionality This file implements the server side of the application.
  */
