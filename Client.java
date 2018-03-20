b62f3025b4578fd764be5c4085fd32b0212d5913
Daniel Christopher Hirt <dhirt@uncc.edu>
2018-03-20 15:27:01 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package midterm;
+package app;
 
 /**
  *
  * @author Daniel Hirt
- * @description ITSC 3166 Midterm Project
+ * @description Client/Server App
  * @file Client.java
  * @functionality This file implements the client side of the application.
  */
